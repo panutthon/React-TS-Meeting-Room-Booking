73899298a556a92fa1310963b787b300c7219823
panutthon <93002896+panutthon@users.noreply.github.com>
2024-04-25 11:52:33 +0700

    setting Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux-toolkit/store.ts";
+
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
@@ -8,9 +10,11 @@ import "./global.css";
 import router from "./routes/root.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
     <ChakraProvider>
       <RouterProvider router={router} />
     </ChakraProvider>
-  </React.StrictMode>
+    {/* </React.StrictMode> */}
+  </Provider>
 );
