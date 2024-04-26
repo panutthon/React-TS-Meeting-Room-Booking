fb9cee55050b6474a384fdd76dc906abea3c9ea2
panutthon <93002896+panutthon@users.noreply.github.com>
2024-04-26 16:41:01 +0700

    token profile set

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { LoginResponse } from "../app-types/login.type"
+import { ProfileResponse } from "../app-types/profile.type";
 import { AxiosResponse, http } from "./http.service"
 
 
@@ -14,6 +15,15 @@ export function logout(): void{
     localStorage.removeItem('token');
 }
 
-export async function getProfile() {
-    
+export async function getProfile(): Promise<AxiosResponse<ProfileResponse> | null> {
+    const token = JSON.parse(localStorage.getItem('token')!) as LoginResponse;
+    if (!token) {
+        return null;
+    }
+    return await http.get<ProfileResponse>('https://api.codingthailand.com/api/profile',{
+        headers: {
+            Authorization: 'Bearer ' + token.access_token
+            // Authorization: `Bearer ${token.access_token}`
+        }
+    });
 }
